80221994274ed3f9d0d68932713fee5f7e522d9a
joao vitor xarrua dos santos <joaoxarrua137@gmail.com>
2024-04-26 10:31:08 -0300

    time added

--- src/Components/Game/GameHeader.jsx
+++ src/Components/Game/GameHeader.jsx
@@ -3,6 +3,7 @@ import IconReturn from '../../assets/IconReturn'
 import IconMenu from '../../assets/IconMenu'
 import RoundedButton from '../RoundedButton/RoundedButton'
 import { useNavigate } from 'react-router-dom'
+import Timer from '../Timer/Timer'
 
 const GameHeader = () => {
   const navigator = useNavigate();
@@ -13,7 +14,7 @@ const GameHeader = () => {
           <IconReturn />
         </RoundedButton>
         <div className='text-white'>
-            <span className='text-2xl'>00:00</span>
+            <Timer />
         </div>
         <RoundedButton active={true} href="#">
           <IconMenu />
